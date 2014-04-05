5c6cb5391d9ecf625fafe5fdfed61ade8fd6933a
Chris Johnson <chris@johnsonch.com>
2014-04-04 23:17:34 -0500

    backup db

--- lib/command.rb
+++ lib/command.rb
@@ -1,5 +1,6 @@
 class Command 
   def self.run(command)
+    puts "Running: #{command}"
     puts `#{command}`
   end
 end
--- lib/ssh.rb
+++ lib/ssh.rb
@@ -2,4 +2,8 @@ class SSH
   def self.command_builder(config, host, command='')
    "ssh -F #{config} #{host} '#{command}'" 
   end
+
+  def self.scp(config, host, remote_file)
+    "scp -F #{config} #{host}:~/#{remote_file} ."
+  end
 end
