ddf90bc2435b43e6712730fec990b94aebb6aed3
Chris Johnson <chris@johnsonch.com>
2014-04-08 21:30:20 -0500

    some examples from live coding

--- lib/ssh.rb
+++ lib/ssh.rb
@@ -3,7 +3,11 @@ class SSH
    "ssh -F #{config} #{host} '#{command}'" 
   end
 
-  def self.scp(config, host, remote_file)
+  def self.scp_from(config, host, remote_file)
     "scp -F #{config} #{host}:~/#{remote_file} ."
+  end
+
+  def self.scp_to(config, host, file, remote_location='')
+    "scp -F #{config} #{file} #{host}:~/#{remote_location}"
   end
 end
